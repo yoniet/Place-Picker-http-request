0c925b0385c5403552c89850897f70cee615c889
YoniEtedgi <yonie812@gmail.com>
2024-01-22 17:59:27 +0200

    new update for custome React hook

--- src/components/Places.jsx
+++ src/components/Places.jsx
@@ -1,5 +1,5 @@
 export default function Places({ title, places, fallbackText, onSelectPlace, isLoading, loadingText }) {
-  console.log(places);
+
   return (
     <section className="places-category">
       <h2>{title}</h2>
