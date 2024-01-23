b46c4481d944f682b621a8a6e33c6b951fd01faa
YoniEtedgi <yonie812@gmail.com>
2024-01-23 19:57:51 +0200

    app.js updated with a custom hook to fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,40 @@ import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
 import { fetchUserPlaces, updateUserPlaces } from './http.js';
 import Error from './components/Error.jsx';
+import { useFetch } from './hooks/useFetch.js';
 
 function App() {
   const selectedPlace = useRef();
 
-  const [userPlaces, setUserPlaces] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-  const [error, setError] = useState();
+  //  I use with custom hook
+  // const [userPlaces, setUserPlaces] = useState([]);
+  // const [isFetching, setIsFetching] = useState(false);
+  // const [error, setError] = useState();
 
 
   const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
 
+  //  I use with custom hook
+  // useEffect(() => {
+  //   async function fetchPlaces() {
+  //     setIsFetching(true);
+  //     try {
+  //       const places = await fetchUserPlaces();
+  //       setUserPlaces(places);
+  //     } catch (error) {
+  //       setError({ message: error.message || 'Failed to fetch user places.' })
+  //     }
+  //     setIsFetching(false);
+  //   }
 
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
-      try {
-        const places = await fetchUserPlaces();
-        setUserPlaces(places);
-      } catch (error) {
-        setError({ message: error.message || 'Failed to fetch user places.' })
-      }
-      setIsFetching(false);
-    }
+  //   fetchPlaces();
+  // }, []);
 
-    fetchPlaces();
-  }, []);
+  // custom hook to fetch data (here places)
+  const { isFetching, fetchedData: userPlaces,setFetchedData:setUserPlaces, error } = useFetch(fetchUserPlaces, []); 
+
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
