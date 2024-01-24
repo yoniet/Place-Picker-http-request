4aab4d8649d24ca7f3cda259e3765b990e382719
YoniEtedgi <yonie812@gmail.com>
2024-01-24 07:58:08 +0200

    AvailablePlaces.jsx updated with a custom hook to fetch data

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,45 +1,69 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 import Error from './Error.jsx';
 import { sortPlacesByDistance } from '../loc.js';
 import { fetchAvailablePlaces } from '../http.js';
+import { useFetch } from '../hooks/useFetch.js'
+
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces();
+
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      resolve(sortedPlaces);
+    });
+  });
+}
+
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [isFetching, setIsFetching] = useState(false);
-  const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [error, setError] = useState();
+  // const [isFetching, setIsFetching] = useState(false);
+  // const [availablePlaces, setAvailablePlaces] = useState([]);
+  // const [error, setError] = useState();
 
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
-      try {
-        const places = await fetchAvailablePlaces();
+  const {
+    isFetching,
+    error,
+    fetchedData: availablePlaces,
+  } = useFetch(fetchSortedPlaces, []);
 
-        navigator.geolocation.getCurrentPosition((position) => {
-          const sortedPlaces = sortPlacesByDistance(
-            places,
-            position.coords.latitude,
-            position.coords.longitude
-          );
-          setAvailablePlaces(sortedPlaces);
-          setIsFetching(false);
-        });
 
-      } catch (error) {
-        setError({ message: error.message || 'Could not fetch places, please try again later' });
-        setIsFetching(false);
-      }
+  // useEffect(() => {
+  //   async function fetchPlaces() {
+  //     setIsFetching(true);
+  //     try {
+  //       const places = await fetchAvailablePlaces();
 
-    }
-    fetchPlaces();
+  //       navigator.geolocation.getCurrentPosition((position) => {
+  //         const sortedPlaces = sortPlacesByDistance(
+  //           places,
+  //           position.coords.latitude,
+  //           position.coords.longitude
+  //         );
+  //         setAvailablePlaces(sortedPlaces);
+  //         setIsFetching(false);
+  //       });
 
-    // fetch('http://localhost:3000/places')
-    // .then((response)=> {
-    //   return response.json();
-    // }).then((resData)=> {
-    //   setAvailablePlaces(resData.places);
-    // });
-  }, [])
+  //     } catch (error) {
+  //       setError({ message: error.message || 'Could not fetch places, please try again later' });
+  //       setIsFetching(false);
+  //     }
+
+  //   }
+  //   fetchPlaces();
+
+  // fetch('http://localhost:3000/places')
+  // .then((response)=> {
+  //   return response.json();
+  // }).then((resData)=> {
+  //   setAvailablePlaces(resData.places);
+  // });
+  // }, [])
 
   if (error) {
     return (
